45ec0361d6155a123edb6a3d1644327522da87fc
Sebxn <snaranjo@utem.cl>
2025-05-18 00:48:49 -0400

    add Permisos SSL para render

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,12 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod', // <-- para render postgres
+      extra: {
+        ssl: process.env.STAGE === 'prod'
+              ? { rejectUnauthorized: false }
+              : null,
+      },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
