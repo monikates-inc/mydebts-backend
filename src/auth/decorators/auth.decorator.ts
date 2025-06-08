1a0f39eb9e6ee62af504d52255e90d3287200cd1
Sebxn <snaranjo@utem.cl>
2025-06-08 00:17:55 -0400

    decorador para verificar logeo

---
+++ src/auth/decorators/auth.decorator.ts
@@ -0,0 +1,12 @@
+import { applyDecorators, UseGuards } from '@nestjs/common';
+
+import { AuthGuard } from '@nestjs/passport';
+
+
+export function Auth() {
+
+    return applyDecorators(
+
+        UseGuards(AuthGuard()),
+    );
+}
