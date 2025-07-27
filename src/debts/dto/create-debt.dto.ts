cfb0599bb0c4dc4d8f105e436ec3e191e5db5ed5
Sebxn <snaranjo@utem.cl>
2025-07-27 19:51:10 -0400

    Documentacion swaggger

--- src/debts/dto/create-debt.dto.ts
+++ src/debts/dto/create-debt.dto.ts
@@ -1,21 +1,42 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNumber, IsOptional, IsPositive, IsString, Matches, MinLength } from "class-validator";
 
 export class CreateDebtDto {
 
     // info del deudor
+    @ApiProperty({
+        description: 'Nombre del deudor',
+        nullable: false,
+        minLength: 1
+    })
     @IsString()
     @MinLength(1)
     name: string;
 
+    @ApiProperty({
+        description: 'Apellido del deudor',
+        nullable: false,
+        minLength: 1
+    })
     @IsString()
     @MinLength(1)
     lastname: string;
 
+    @ApiProperty({
+        description: 'Email del deudor',
+        nullable: true,
+        example: 'correo@correo.com'
+    })
     @IsString()
     @IsEmail()
     @IsOptional()
     email?: string;
 
+    @ApiProperty({
+        description: 'Teléfono del deudor',
+        nullable: true,
+        example: '+56212345678'
+    })
     @IsString()
     @IsOptional()
     @Matches(/^\+56[2-9]\d{8}$/, {
@@ -24,7 +45,20 @@ export class CreateDebtDto {
     phone?: string;   
 
     //info de la deuda
+    @ApiProperty({
+        description: 'Monto de la deuda',
+        nullable: false,
+        minLength: 1
+    })
     @IsNumber()
     @IsPositive()
     mount: number;
+
+    @ApiProperty({
+        description: 'Descripción de la deuda',
+        nullable: true,
+    })
+    @IsString()
+    @IsOptional()
+    description?: string;
 } 
